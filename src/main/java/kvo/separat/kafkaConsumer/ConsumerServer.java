e1a395e1d1eee11d059c0f13b20b1b360711827e
alexkv2000 <alexkv2000@mail.ru>
2025-09-24 13:11:11 +0300

    Add metric for Prometheus

--- src/main/java/kvo/separat/kafkaConsumer/ConsumerServer.java
+++ src/main/java/kvo/separat/kafkaConsumer/ConsumerServer.java
@@ -1,11 +1,24 @@
 package kvo.separat.kafkaConsumer;
 
+import io.prometheus.client.Histogram;
 import kvo.separat.mssql.MSSQLConnection;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.prometheus.client.CollectorRegistry;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.exporter.HTTPServer;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -26,8 +39,11 @@ import java.util.stream.StreamSupport;
 import static java.lang.Thread.sleep;
 
 public class ConsumerServer {
-
+    private static PrometheusMeterRegistry registery;
     private static final Logger logger = LoggerFactory.getLogger(ConsumerServer.class);
+
+    private static HTTPServer metricsServer;
+
     private final KafkaConsumerWrapper kafkaConsumer;
     private final DatabaseService databaseService;
     private final EmailService emailService;
@@ -43,7 +59,79 @@ public class ConsumerServer {
     private final String userMssql;
     private final String passwordMssql;
     private int deleteAfterDay;
+    private static Counter messagesConsumed;
+    private static Counter messagesProcessed;
+    private static Timer messagesProcessedTimer;
+    private static Counter messagesSendingFailed;
+    private static Timer messagesConsumerTimer;
+    private static Counter messagesConsumerFailed;
+    private static Counter messagesProcessedFailed;
+    private static Histogram processingLatency;
+    private static void initializeMonitoring() {
+        try {
+            // Создание Prometheus registry
+            registery = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
 
+            // Инициализация метрик
+            messagesConsumed = Counter.builder("consumer_messages_consumed_total")
+                    .description("Total number consumer messages")
+                    .tag("application","ConsumerServer")
+                    .register(registery);
+
+            messagesProcessed = Counter.builder("messages_sent_total")
+                    .description("Total number sent messages")
+                    .tag("application","ConsumerServer")
+                    .register(registery);
+
+           messagesSendingFailed = Counter.builder("messages_sent_failed_total")
+                   .description("Total number failed sent messages")
+                   .tag("application","ConsumerServer")
+                   .register(registery);
+
+            messagesConsumerTimer = Timer.builder("messages_duration_seconds")
+                    .description("Messages synchronization duration in seconds")
+                    .tag("application","ConsumerServer")
+                    .register(registery);
+            messagesProcessedTimer = Timer.builder("messages_sent_failed_total")
+                    .description("Total number Failed sent messages")
+                    .tag("application","ConsumerServer")
+                    .register(registery);
+            messagesConsumerFailed = Counter.builder("consumer_messages_failed_total")
+                    .description("Total number failed consumer messages")
+                    .tag("application","ConsumerServer")
+                    .register(registery);
+            messagesProcessedFailed = Counter.builder("messages_sent_processed_failed_total")
+                    .description("Total number failed processed messages")
+                    .tag("application","ConsumerServer")
+                    .register(registery);
+// Инициализация Histogram через Prometheus client
+            processingLatency = Histogram.build()
+                    .name("processing_latency_seconds")
+                    .help("Latency of message processing in seconds.")
+                    .register();
+
+            // Биндеры для мониторинга JVM
+            new JvmMemoryMetrics().bindTo(registery);
+            new JvmGcMetrics().bindTo(registery);
+            new JvmThreadMetrics().bindTo(registery);
+            new ProcessorMetrics().bindTo(registery);
+            new UptimeMetrics().bindTo(registery);
+
+            // Запуск HTTP сервера для Prometheus
+            //     ДОБАВЛНО ВМЕСТО  -  HTTPServer server = new HTTPServer(8080);
+            CollectorRegistry collectorRegistry = registery.getPrometheusRegistry();
+
+            metricsServer = new HTTPServer.Builder()
+                    .withPort(8080)
+                    .withRegistry(collectorRegistry)
+                    .build();
+
+            logger.info("sync-app metrics server started on http://......:8080/metrics");
+
+        } catch (Exception e) {
+            logger.error("Failed to initialize monitoring in sync-app", e);
+        }
+    }
     public ConsumerServer(KafkaConsumerWrapper kafkaConsumer, DatabaseService databaseService,
                           EmailService emailService, ConfigLoader configLoader) {
         this.kafkaConsumer = kafkaConsumer;
@@ -60,73 +148,79 @@ public class ConsumerServer {
         this.urlMssql = configLoader.getProperty("URL_MSSQL");
         this.userMssql = configLoader.getProperty("USER_MSSQL");
         this.passwordMssql = configLoader.getProperty("PASSWORD_MSSQL");
-        deleteAfterDay = Integer.parseInt(configLoader.getProperty("DELETE_AFTER_DAY"));
+        this.deleteAfterDay = Integer.parseInt(configLoader.getProperty("DELETE_AFTER_DAY"));
     }
 
     public void startProcessing() {
+        initializeMonitoring();
         scheduler.scheduleWithFixedDelay(() -> {
-            try {
-                Connection connection = DriverManager.getConnection(urlMssql, userMssql, passwordMssql);
-                MSSQLConnection.deleteBinMoreSevenDays(connection, java.time.LocalDate.now());  // Удаление Binary из MSSQL больше 7 дней
-                DatabaseService.deleteOldMessages(deleteAfterDay); // Уделание Messages из MySQL больше 7 дней
+            try (Connection connection = DriverManager.getConnection(urlMssql, userMssql, passwordMssql)) {
+                MSSQLConnection.deleteBinMoreSevenDays(connection, java.time.LocalDate.now());
+                DatabaseService.deleteOldMessages(deleteAfterDay);
             } catch (Exception e) {
-                logger.error("Error in ConsumerServer.startProcessing.MSSQLConnection.deleteBinMoreSevenDay ", e);
+                logger.error("Error in MSSQL and DB cleanup tasks", e);
             }
         }, 0, 1, TimeUnit.DAYS);
 
         scheduler.scheduleWithFixedDelay(() -> {
+            Timer.Sample sample = Timer.start(registery); // Регистрируем Метрику
             try {
-                this.setKafkaConsumer();
+                setKafkaConsumer();
             } catch (Exception e) {
-                logger.error("Error in ConsumerServer.startProcessing.setKafkaConsumer", e);
+                logger.error("Error in setKafkaConsumer", e);
+                messagesConsumerFailed.increment(); // ошибка получения сообщения KAFKA
             }
+            sample.stop(messagesConsumerTimer); // Стоп Метрика
         }, 0, 5, TimeUnit.SECONDS);
 
         scheduler.scheduleWithFixedDelay(() -> {
+            Timer.Sample sample = Timer.start(registery); // Регистрируем Метрику
             try {
-                this.processMessages();
+                processMessages();
             } catch (Exception e) {
-                logger.error("Error in ConsumerServer.startProcessing.processMessages", e);
+                logger.error("Error in processMessages", e);
+                messagesProcessedFailed.increment(); // ошибка отправки сообщения
             }
+            sample.stop(messagesProcessedTimer); // Стоп Метрика
         }, 0, 5, TimeUnit.SECONDS);
+
     }
 
     private void setKafkaConsumer() {
-        logger.debug("Start setKafkaConsumer: getConsumerRecords()");
+        logger.debug("Polling Kafka records...");
         List<ConsumerRecord<String, String>> recordList = getConsumerRecords();
-        logger.debug("Start setKafkaConsumer: AddCorrectDataJSONFromBrokerToDBSQL()");
         addCorrectDataJSONFromBrokerToDBSQL(recordList);
     }
 
     private void processMessages() {
         try {
-            logger.debug("Поиск данных сообщений из БД");
+            logger.debug("Updating messages status to 'select' for processing");
             databaseService.updateMessagesForProcessing(topic, server, "select", typeMes, limitSelect);
 
-            logger.debug("Выборка данных сообщений из БД");
+            logger.debug("Selecting messages for processing");
             List<MessageData> resultSet = databaseService.selectMessages(topic, server, typeMes, limitSelect);
 
             if (resultSet == null || resultSet.isEmpty()) {
-                logger.debug("Не сообщений в обработке");
+                logger.debug("No messages to process");
                 return;
             }
 
             List<CompletableFuture<Void>> futures = new ArrayList<>();
 
-            for (MessageData result : resultSet) {
-                if (!isValidMessage(result)) {
-                    logger.error("Некорректные данные в сообщении с ID: {}", result != null ? result.getId() : "null");
-                    updateMessageStatusWithError(result, "error DATA JSON");
+            for (MessageData message : resultSet) {
+                if (!isValidMessage(message)) {
+                    logger.error("Invalid message data with ID: {}", message != null ? message.getId() : "null");
+                    updateMessageStatusWithError(message, "error DATA JSON");
                     continue;
                 }
 
-                futures.add(processSingleMessageAsync(result));
+                futures.add(processSingleMessageAsync(message));
             }
 
             CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 
         } catch (Exception e) {
-            logger.error("Критическая ошибка в обработке сообщений ", e);
+            logger.error("Critical error during message processing", e);
         }
     }
 
@@ -141,10 +235,11 @@ public class ConsumerServer {
             try {
                 Files.createDirectories(dir);
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.error("Failed to create directory: {}", filePath, e);
             }
         }
         return CompletableFuture.runAsync(() -> {
+            Histogram.Timer timer = processingLatency.startTimer();
             try {
                 StringBuilder filePathBuilder = MSSQLConnection.DownloadBinaryDV(
                         message.getUuid(), urlMssql, userMssql, passwordMssql, filePath);
@@ -155,15 +250,20 @@ public class ConsumerServer {
                 cleanupTempFiles(String.valueOf(message.getUuid()));
                 updateMessageStatus(String.valueOf(message.getId()), "send");
 
+                messagesProcessed.increment();
+
             } catch (IOException e) {
-                logger.error("IO error процесса сообщения с ID: {}", message.getId(), e);
+                messagesSendingFailed.increment();
+                logger.error("IO error processing message ID: {}", message.getId(), e);
                 updateMessageStatusWithError(message, "IO error");
+            } finally {
+                timer.observeDuration();
             }
         }, executor);
     }
 
-    private void cleanupTempFiles(String uuid) throws IOException {
-        Path tempDir = Path.of(filePath + uuid);
+    private void cleanupTempFiles(String uuid) {
+        Path tempDir = Paths.get(filePath + uuid);
         if (Files.exists(tempDir)) {
             MSSQLConnection.deleteDirectory(UUID.fromString(uuid), filePath);
         }
@@ -234,6 +334,7 @@ public class ConsumerServer {
                     String extractedTypeMes = json.getString("typeMes");  // Извлекаем значение {typeMes} как строку
                     String extractedUUID = json.getString("uuid"); // Извлекаем значение {uuid} как строку
                     databaseService.insertMessages(Collections.singletonList(record), extractedTypeMes, extractedUUID);
+                    messagesConsumed.increment();
                 } else {
                     logger.warn("Ключ 'typeMes' отсутствует в JSON-сообщении: {}", record.value());
                 }
